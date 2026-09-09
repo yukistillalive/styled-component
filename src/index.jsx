e1c42066326e1961dabe78cc21be4dadea0bd655
Yue Zhu <zhu5@kth.se>
2026-09-09 14:19:40 +0200

    create basic app demo

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 // TODO make a reactive model (application state), pass it as prop to the components used
 import { createRoot } from "react-dom/client";
-import App from "./Demo3.jsx";
+import App from "./App.jsx";
 
 const mountedApp = createRoot(document.getElementById("root"));
 mountedApp.render(<App />);
